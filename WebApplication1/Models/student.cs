9fc1e4e819b38ab2bf10f0fae357dfb1e9134284
孙嘉良 <@DESKTOP-24EOMCR>
2022-04-21 16:00:36 +0800

    添加项目文件。

---
+++ WebApplication1/Models/student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class student
+    {
+        public int Id { get; set; }
+        public string? Nameuse { get; set; }
+        public string? Gender { get; set; }
+        public int Age { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? Brith { get; set; }
+        public string? phon { get; set; }
+        public float? Tem { get; set; }
+        public DateTime? Datetime { get; set; }
+
+    }
+}
